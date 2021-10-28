7615f09023b69cb38ec19b81711a88a399b234f8
Sidney DuPont <sidneyadupont@gmail.com>
2021-10-27 19:10:44 -0700

    Passes max_subarray test with Kadane algorithm

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -2,11 +2,20 @@
 def max_sub_array(nums):
     """ Returns the max subarray of the given list of numbers.
         Returns 0 if nums is None or an empty list.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(n)
     """
     if nums == None:
         return 0
     if len(nums) == 0:
         return 0
-    pass
+
+    best_sum = 0
+    current_sum = 0
+
+    for num in nums:
+        current_sum = max(0, current_sum + num)
+        best_sum = max(best_sum, current_sum)
+    if best_sum <= 0:
+        return max(nums)
+    return best_sum
