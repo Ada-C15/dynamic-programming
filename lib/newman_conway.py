4dd05fa8096e338bb426a416ef12e4bb7159567a
Glenda <chicag9313@gmail.com>
2021-10-30 16:30:18 -0700

    Delete un used code

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,7 +1,3 @@
-
-import array
-# Time complexity: ?
-# Space Complexity: ?
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
         Time Complexity: O(n)
