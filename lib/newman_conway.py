7bfbe8d3bf4ffaa5de1a3e646a534bf31314f1aa
Leilani Allen <leilani.allen90@gmail.com>
2021-10-30 16:30:14 -0700

    All tests passing

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,10 +1,21 @@
 
-
-# Time complexity: ?
-# Space Complexity: ?
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(n)
     """
-    pass
+    # if number is zero, throw error
+    if num == 0:
+        raise ValueError("can't be zero")
+    # if number is 1, just return '1' or first num in sequence
+    if num == 1:
+        return "1"
+    # Declare array to store sequence 
+    num_array = [0, 1, 1] 
+
+    for i in range(3,num+1): 
+        num_array.append( num_array[num_array[i - 1]] + num_array[i - num_array[i - 1]])
+
+    # turning array back into a string of numbers
+    string = ' '.join([str(item) for item in num_array[1:]])
+    return string
