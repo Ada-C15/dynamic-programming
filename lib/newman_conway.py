fc24fea7f158d4ae4ad7379659dd3e2096c76676
Tammy <iptmhatuw@gmail.com>
2021-10-30 22:26:19 -0700

    pass all tests

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,10 +1,23 @@
 
 
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n)
+# Space Complexity: O(n)
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(n)
     """
-    pass
+    if num == 0:
+        raise ValueError("num cannot be zero")
+
+    if num == 1:
+        return "1"
+
+    arr = [0, 1, 1] 
+
+    for i in range(3, num+1): 
+        arr.append(arr[arr[i-1]] + arr[i - arr[i-1]]) 
+
+    string = ' '.join([str(item) for item in arr[1:]])
+
+    return string 
