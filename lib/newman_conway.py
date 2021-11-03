d0d9526f6a4cc5a517b76d386b2e400e633bae36
Jennifer Lim <jennifer.lim@redfin.com>
2021-11-02 20:25:53 -0700

    passing tests

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -4,7 +4,20 @@
 # Space Complexity: ?
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(n)
     """
-    pass
+
+    if num == 0:
+        raise ValueError
+
+    if num == 1:
+        return '1'
+
+    sequence = [0, 1, 1]
+    for i in range(3, num+1):
+        nc = sequence[sequence[i - 1]] + sequence[i - sequence[i - 1]]
+        sequence.append(nc)
+    
+    return ' '.join([str(s) for s in sequence[1:]])
+
