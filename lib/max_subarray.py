83725c1bcde33b88a51c3e56d1820d6570040430
Ari <ari.go.org.7@gmail.com>
2021-11-02 22:25:01 -0700

    all tests ok

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -1,3 +1,4 @@
+from math import inf 
 
 def max_sub_array(nums):
     """ Returns the max subarray of the given list of numbers.
@@ -5,8 +6,27 @@ def max_sub_array(nums):
         Time Complexity: ?
         Space Complexity: ?
     """
-    if nums == None:
+    # if nums == None:
+    if not nums or len(nums) == 0:
         return 0
-    if len(nums) == 0:
-        return 0
-    pass
+
+    sum_so_far = 0  # from wherever we starting to where we are (array)
+    # max_sum_so_far = 0      # nums[i]
+    max_sum_so_far = float('-inf')  # negative infinity!
+    for num in nums:
+        sum_so_far += num
+        # check if it's worth including the number
+        if sum_so_far > max_sum_so_far:
+            max_sum_so_far = sum_so_far
+        # elif sum_so_far < 0:
+        #     # sum_so_far = 0  # start algorithm over
+        #     sum_so_far = sum_so_far + num
+        elif sum_so_far < 0:
+            max_sum_so_far = num
+    return max_sum_so_far
+
+
+# [10, -20, 30, 3, -50, 10]
+
+# biggest number we added so far
+# sum so far
