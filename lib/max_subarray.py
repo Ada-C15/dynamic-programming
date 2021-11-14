dc97e64bcd7b8f24ae87567dec37504fab82cdb5
Ruthie <ruthienewman@protonmail.com>
2021-11-14 13:08:43 -0800

    adds functionality to newman-conway

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -5,8 +5,32 @@ def max_sub_array(nums):
         Time Complexity: ?
         Space Complexity: ?
     """
+
+    #https://www.geeksforgeeks.org/largest-sum-contiguous-subarray/
+    #https://www.youtube.com/watch?v=ohHWQf1HDfU
+
+    max_sum = 0
+    current_max_sum = 0
+
     if nums == None:
         return 0
     if len(nums) == 0:
         return 0
-    pass
+
+    #logic:
+    #set the new calc to previous until you determine if it is <> then previous
+
+    #loop through nums 
+    for i in range(len(nums)-1):
+        print(f"current_max_sum: {current_max_sum}")
+        current_max_sum = current_max_sum + nums[i]
+        print(f"current_max_sum: {current_max_sum}")
+    
+    if max_sum < current_max_sum:
+        max_sum = current_max_sum
+        print(f"max_sum: {max_sum}")
+    
+    return max_sum
+
+
+    
--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,10 +1,56 @@
+#Newman Conway Sequence is the sequence which follows a 
+#given recursive relation (p(n) = p(p(n-1)) + p(n-p(n-1))) 
+#to generate a series of numbers. 
 
+# Time complexity: ? Should be O(n) becuase processing time will depend on the size of n
+# Space Complexity: Space complexity depends on how many memo's are taken? So O(n) maybe?
 
-# Time complexity: ?
-# Space Complexity: ?
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    
+    #base case?
+    if (num <= 0):
+        #raise a value error?
+        raise ValueError
+
+    #create memoization array based on the input num
+    memo = [0] * (num+1)
+    print(f"memo initial: {memo}")
+
+    #set inital values
+    memo[0] = 0
+    memo[1] = 1
+    # memo[2] = 1
+    print(f"memo: {memo}")
+
+    # #im not sure how to deal with the cases of 1 and 2
+    if num == 1:
+        return '1'
+    
+    # #base case
+    if (num == 2) :
+        memo[2] = 1
+        return '1 1'
+
+    for i in range(3, num + 1):
+        print(f"num: {num}")
+        #(p(n) = p(p(n-1)) + p(n-p(n-1))) 
+        memo[i] = memo[memo[num-1]] + memo[num - memo[num - 1]]
+        print(f"new num: {num}")
+        print(f"memo after loop: {memo}")
+    
+    return memo
+
+    
+    # i = 3
+    # while (i <= num):
+    # #collect nc sequence numbers
+    #     memo[i] = memo[memo[i-1]] + memo[i - memo[i -1]]
+    #     i += 1
+    
+    #return memo
+
+    
