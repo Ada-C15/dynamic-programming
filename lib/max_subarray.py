69653c5f03e663d23e568d1e00075994a0bd650e
mairandomness <mairakodama@gmail.com>
2021-11-18 14:29:08 -0800

    Add solutions

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -2,11 +2,25 @@
 def max_sub_array(nums):
     """ Returns the max subarray of the given list of numbers.
         Returns 0 if  nums is None or an empty list.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(1)
     """
-    if nums == None:
+    if not nums:
         return 0
-    if len(nums) == 0:
-        return 0
-    pass
+    
+    if max(nums) < 0:
+        return max(nums)
+    
+    max_seen = 0
+    max_ending_here = 0
+
+    for num in nums:
+        max_ending_here += num
+
+        if max_ending_here < 0:
+            max_ending_here = 0
+                
+        if max_seen < max_ending_here:
+            max_seen = max_ending_here
+
+    return max_seen
