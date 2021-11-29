d9fe64628533d52b9252bb9675dccbd2ddc5dc81
Nataliia Getlin <natali.getlin@gmail.com>
2021-11-28 21:33:50 -0800

    Complete assignment

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -2,11 +2,17 @@
 def max_sub_array(nums):
     """ Returns the max subarray of the given list of numbers.
         Returns 0 if  nums is None or an empty list.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(1)
     """
-    if nums == None:
+    if not nums or len(nums) == 0:
         return 0
-    if len(nums) == 0:
-        return 0
-    pass
+
+    global_max = float("-inf")
+    current_max = float("-inf")
+
+    for num in nums:
+        current_max = max(num + current_max, num)
+        global_max = max(current_max, global_max)
+
+    return global_max
--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,10 +1,26 @@
-
-
-# Time complexity: ?
-# Space Complexity: ?
+# Time complexity: O(n)
+# Space Complexity: O(n)
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+
+        P(1) = 1
+        P(2) = 1
+        for all n > 2
+        P(n) = P(P(n - 1)) + P(n - P(n - 1))
+
+        Time Complexity: O(n)
+        Space Complexity: O(n)
     """
-    pass
+
+    sequence = [None, 1, 1]
+
+    if num < 1:
+        raise ValueError
+
+    index = 3
+    while index <= num:
+        sequence.append(sequence[sequence[index - 1]] +
+                        sequence[index - sequence[index - 1]])
+        index += 1
+
+    return " ".join([str(element) for element in sequence[1:num + 1]])
