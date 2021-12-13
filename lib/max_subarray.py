2de2fab90e170efb0a6492a315978e27479d6ce1
chanbzz <cboswell1024@gmail.com>
2021-12-12 19:31:00 -0500

    max_subarray tests passing'

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -9,4 +9,25 @@ def max_sub_array(nums):
         return 0
     if len(nums) == 0:
         return 0
-    pass
+    if len(nums) == 1:
+        return nums[0]
+
+    else:
+        max = nums[0]
+        max_array = 0
+        sub_array_sum = 0
+
+        for num in nums:
+            max_array += num
+            sub_array_sum += num
+            if num > max:
+                max = num
+            if sub_array_sum > max_array:
+                max_array = sub_array_sum
+            elif max_array > max:
+                max = max_array
+        return max
+        
+        
+            
+
