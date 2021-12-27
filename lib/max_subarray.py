bf30a40f4bc2bf9327b64149d88611e05b759ce1
lisa1501 <halisasaipulla@gmail.com>
2021-12-27 13:22:41 -0800

    passed all tests

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -2,11 +2,28 @@
 def max_sub_array(nums):
     """ Returns the max subarray of the given list of numbers.
         Returns 0 if  nums is None or an empty list.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(n)
     """
-    if nums == None:
+    size = len(nums)
+    if size == 0:
         return 0
-    if len(nums) == 0:
-        return 0
-    pass
+    dp = [0 for _ in range(size)]
+    for i,num in enumerate(nums):            
+        dp[i] = max(dp[i-1] + num, num)
+    return max(dp)
+
+
+    # if nums == None:
+    #     return 0
+
+    # if len(nums) == 0:
+    #     return 0
+
+    # max_so_far =nums[0]
+    # curr_max = nums[0]
+    
+    # for i in range(1,len(nums)):
+    #     curr_max = max(nums[i], curr_max + nums[i])
+    #     max_so_far = max(max_so_far,curr_max)
+    # return max_so_far
--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -4,7 +4,31 @@
 # Space Complexity: ?
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n^2)
+        Space Complexity: O(n)
     """
-    pass
+    if num == 0:
+        raise ValueError("ValueError")
+    if num == 1:
+        return '1'
+    s='1 1'
+    arr =[0,1,1]
+    
+    for num in range(3,num+1):
+        new_num = arr[arr[num-1]]+arr[num -arr[num-1]]
+        # i=3 arr[arr[2]]+arr[3-arr[2]]=arr[1]+arr[2]=2
+        arr.append(new_num)
+        s += " "+str(new_num)+''
+    return s
+
+
+
+
+    
+
+
+    
+
+
+
+
