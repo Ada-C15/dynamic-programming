40371d4d98354b15dbb3b9c4eb0c8f71140f2da1
Hena <lee.henajane@gmail.com>
2022-01-04 01:16:11 -0600

    Passes all tests

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -4,7 +4,18 @@
 # Space Complexity: ?
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n) single for loop to find nth element of sequence - linear
+        Space Complexity: O(n) use list to store cache which is required to compute nth element - linear
     """
-    pass
+    if num == 0:
+        raise ValueError("Number cannot be zero")
+    elif num == 1:
+        return "1"
+
+    cache = [0,1,1]
+    
+    for i in range(3, num + 1): 
+        cache.append(cache[cache[i-1]] + cache[i - cache[i-1]]) 
+    string = ' '.join([str(item) for item in cache[1:]])
+    
+    return string 
