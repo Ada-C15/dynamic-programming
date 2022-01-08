e11904b6cf405d463fe71b25d82d37d2db029706
Courtney <courtneycmassey@gmail.com>
2022-01-08 13:42:35 -0800

    passing nc tests

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -3,8 +3,22 @@
 # Time complexity: ?
 # Space Complexity: ?
 def newman_conway(num):
-    """ Returns a list of the Newman Conway numbers for the given value.
+    """ Returns a string of the Newman Conway numbers for the given value.
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    if num == 0:
+        raise ValueError
+    
+    if num == 1:
+        return "1"
+
+    seq = [0, 1, 1]
+
+    for i in range(3, num + 1):
+        next_num = seq[seq[i-1]] + seq[i - seq[i-1]]
+        seq.append(next_num)
+    
+    return ' '.join([str(n) for n in seq[1:num+1]])
+
+print(newman_conway(9))
