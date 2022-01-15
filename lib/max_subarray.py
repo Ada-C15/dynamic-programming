f3302ce86b1c4bb78b02d508836f1ceaf0483776
melissa-nguyen <n.melissa.25@gmail.com>
2022-01-14 16:48:35 -0800

    passing tests

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -9,4 +9,16 @@ def max_sub_array(nums):
         return 0
     if len(nums) == 0:
         return 0
-    pass
+    
+    # default both variables to the first index b/c the first index is the max value and max ending thus far
+    max_so_far = nums[0]
+    max_ending_here = nums[0]
+
+    for i in range(1, len(nums)):
+
+        max_ending_here = max(max_ending_here + nums[i], nums[i])
+
+        # continue updating value of variable as we itr
+        max_so_far = max(max_so_far, max_ending_here)
+    
+    return max_so_far
