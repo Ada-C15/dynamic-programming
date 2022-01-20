5937cd85a8788325a7e12565515664eebb0cad9c
Nandita Gilroy <nandita.gilroy@gmail.com>
2022-01-19 17:22:46 -0800

    adds working logic for newman_conway function

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -7,4 +7,19 @@ def newman_conway(num):
         Time Complexity: ?
         Space Complexity: ?
     """
-    pass
+    if num <= 0:
+        raise ValueError("num cannot be less than 1")
+    memo = [0] * (num+1)
+    memo[0] = 0
+    memo[1] = 1
+    if num > 1:
+        memo[2] = 1
+
+    x = 3
+    while x <= num:
+        memo[x] = memo[memo[x - 1]] + memo[x - memo[x - 1]]
+        x += 1
+
+    memo.pop(0)
+    memo = [str(int) for int in memo]
+    return " ".join(memo)
