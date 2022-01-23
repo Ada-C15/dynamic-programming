56402e3df589ede03aa57aa6652769a4c6fd2d50
alison <alimcginley@gmail.com>
2022-01-23 12:42:44 -0700

    completes functions

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -2,11 +2,22 @@
 def max_sub_array(nums):
     """ Returns the max subarray of the given list of numbers.
         Returns 0 if  nums is None or an empty list.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(n)
     """
     if nums == None:
         return 0
     if len(nums) == 0:
         return 0
-    pass
+    
+    maxSum = 0
+    currentSum = 0
+
+    for i in range(len(nums)):
+        currentSum = max((nums[i] + currentSum), nums[i])
+        maxSum = max(currentSum, maxSum)
+    
+    if maxSum <=0:
+        return max(nums)
+    
+    return maxSum
--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -4,7 +4,20 @@
 # Space Complexity: ?
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(n)
     """
-    pass
+    
+    if num == 0:
+        raise ValueError
+    
+    if num == 1:
+        return '1'
+    
+    res = [0,1,1]
+
+    for i in range(3, num+1):
+        sum = res[res[i-1]] + res[i-res[i-1]]
+        res.append(sum)
+
+    return ' '.join(str(num) for num in res[1:])
