88c3fc441fc2d7e9ba45fcae3ba4d91f54455acf
Laurel <lolkinetzky@gmail.com>
2022-01-24 11:38:09 -0700

    chchchchanges

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -2,11 +2,21 @@
 def max_sub_array(nums):
     """ Returns the max subarray of the given list of numbers.
         Returns 0 if  nums is None or an empty list.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)?
+        Space Complexity: O(1)
     """
-    if nums == None:
+    if not nums:
         return 0
-    if len(nums) == 0:
-        return 0
-    pass
+    
+    _max = 0 
+    end = nums[0]
+
+    for num in nums:
+        _max = max(num, _max + num)
+        end = max(end, _max)
+    return end 
+     
+
+
+
+
--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -2,9 +2,24 @@
 
 # Time complexity: ?
 # Space Complexity: ?
-def newman_conway(num):
+def newman_conway(num, memo=None):
     """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)?
+        Space Complexity: O(n)?
     """
-    pass
+    if num == 0:
+        raise ValueError
+
+    if num == 1:
+        return '1'
+
+    if memo is None:
+        memo = [0, 1, 1]
+
+    while num >= len(memo):
+        for i in range(3, num +1):
+            memo.append(memo[memo[i -1]] + memo[i - memo[i - 1]])
+    return ' '.join([str(s) for s in memo[1:]])
+
+
+    
