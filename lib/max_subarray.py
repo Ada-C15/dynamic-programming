24536e5e4bd9337a9da5a26916de9a34cd40f886
Araceli <aracelimaldonado4@yahoo.com>
2022-01-25 13:55:33 -0800

    all tests passing

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -2,11 +2,17 @@
 def max_sub_array(nums):
     """ Returns the max subarray of the given list of numbers.
         Returns 0 if  nums is None or an empty list.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(1)
     """
     if nums == None:
         return 0
     if len(nums) == 0:
         return 0
-    pass
+
+    dp = [0 for i in range(len(nums))]
+    dp[0] = nums[0]
+    for i in range(1,len(nums)):
+        dp[i] = max(dp[i-1]+nums[i],nums[i])
+      #print(dp)
+    return max(dp)
