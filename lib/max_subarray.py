b92d4eedf0600327967ffc799edd63e30602e71f
brijhaywood <briyanahaywood@gmail.com>
2022-01-25 01:22:43 -0600

    Dynamic Programming complete

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -2,11 +2,18 @@
 def max_sub_array(nums):
     """ Returns the max subarray of the given list of numbers.
         Returns 0 if  nums is None or an empty list.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(1)
     """
     if nums == None:
         return 0
     if len(nums) == 0:
         return 0
-    pass
+    current_max = nums[0]
+    max_array = nums[0]
+    
+    for i in range(1, len(nums)):
+        current_max = max(current_max + nums[i], nums[i])
+        max_array = max(max_array, current_max)
+            
+    return max_array 
