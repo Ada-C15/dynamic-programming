8a2036bb1e408f17b1297539399ab39933aff351
Andrea Palacios <andreapalacios88@gmail.com>
2022-01-25 23:17:05 -0800

    newman conway tests passing/ big-o added

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -5,8 +5,9 @@ from re import M
 def max_sub_array(nums):
     """ Returns the max subarray of the given list of numbers.
         Returns 0 if  nums is None or an empty list.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+        
     """
     if nums == None:
         return 0
--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,10 +1,19 @@
 
 
-# Time complexity: ?
-# Space Complexity: ?
+# Time comnewman_conwaylexity: ?
+# Snewman_conwayace Comnewman_conwaylexity: ?
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n)
+        Space Complexity: O(n)
     """
-    pass
+    if num < 1:
+        raise ValueError
+    elif num == 1:
+        return "1"
+    
+    sequence = [0,1,1]
+
+    for i in range(3, num+1):
+        sequence.append(sequence[sequence[i - 1]] + sequence[i - sequence[i - 1]])
+    return (" ".join(map(str, sequence[1:])))
