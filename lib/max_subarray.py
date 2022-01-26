7578028e79632ba2fcc0bfb7312aa3793623c9d2
Andrea Palacios <andreapalacios88@gmail.com>
2022-01-25 22:41:55 -0800

    max subarray passing

--- lib/max_subarray.py
+++ lib/max_subarray.py
@@ -1,4 +1,7 @@
 
+from re import M
+
+
 def max_sub_array(nums):
     """ Returns the max subarray of the given list of numbers.
         Returns 0 if  nums is None or an empty list.
@@ -7,6 +10,18 @@ def max_sub_array(nums):
     """
     if nums == None:
         return 0
-    if len(nums) == 0:
+    if len(nums)==0:
         return 0
-    pass
+
+
+    max_so_far = nums[0]
+    max_ending_here = nums[0]
+
+    for i in range(1,len(nums)):
+        max_ending_here = max_ending_here + nums[i]
+        if(max_ending_here < 0):
+            max_ending_here = max(max_ending_here,nums[i])
+        if(max_so_far < max_ending_here):
+                max_so_far = max_ending_here
+        
+    return max_so_far
