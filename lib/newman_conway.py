40f8fc0b5302c83da97c9ba40c7fc8ce0ff89b63
Ruthie <ruthienewman@protonmail.com>
2022-03-09 22:32:14 -0800

    feat: implement newman_conway method.

--- lib/newman_conway.py
+++ lib/newman_conway.py
@@ -1,56 +1,29 @@
-#Newman Conway Sequence is the sequence which follows a 
-#given recursive relation (p(n) = p(p(n-1)) + p(n-p(n-1))) 
-#to generate a series of numbers. 
-
-# Time complexity: ? Should be O(n) becuase processing time will depend on the size of n
-# Space Complexity: Space complexity depends on how many memo's are taken? So O(n) maybe?
 
 def newman_conway(num):
     """ Returns a list of the Newman Conway numbers for the given value.
-        Time Complexity: ?
-        Space Complexity: ?
+        Time Complexity: O(n) because the number of calculations performed depends on the size of num.
+        
+        Space Complexity: Space complexity is also O(n) becuase newman_conway_nums array to store sequence values, 
+        nm_sequence_without_leading_zero to store result with leading 0 removed and result a array to which the properly 
+        formatted result is saved are created and the amount of space that they occupy will depend on the size of the given num. 
     """
-    
-    #base case?
-    if (num <= 0):
-        #raise a value error?
+    if num == 0:
         raise ValueError
-
-    #create memoization array based on the input num
-    memo = [0] * (num+1)
-    print(f"memo initial: {memo}")
-
-    #set inital values
-    memo[0] = 0
-    memo[1] = 1
-    # memo[2] = 1
-    print(f"memo: {memo}")
-
-    # #im not sure how to deal with the cases of 1 and 2
+    
     if num == 1:
         return '1'
     
-    # #base case
-    if (num == 2) :
-        memo[2] = 1
+    if num == 2:
         return '1 1'
-
+    
+    #array to store sequence values and provide starting values
+    newman_conway_nums = [0, 1, 1]
+    
     for i in range(3, num + 1):
-        print(f"num: {num}")
-        #(p(n) = p(p(n-1)) + p(n-p(n-1))) 
-        memo[i] = memo[memo[num-1]] + memo[num - memo[num - 1]]
-        print(f"new num: {num}")
-        print(f"memo after loop: {memo}")
+        newman_conway_nums.append(newman_conway_nums[newman_conway_nums[i-1]] + newman_conway_nums[i-newman_conway_nums[i-1]])
     
-    return memo
+    nm_sequence_without_leading_zero = [str(num) for num in newman_conway_nums if num != 0]
+    result = " ".join(nm_sequence_without_leading_zero)
+    
+    return result
 
-    
-    # i = 3
-    # while (i <= num):
-    # #collect nc sequence numbers
-    #     memo[i] = memo[memo[i-1]] + memo[i - memo[i -1]]
-    #     i += 1
-    
-    #return memo
-
-    
