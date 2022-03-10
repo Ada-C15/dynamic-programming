657a6c743a5b16cadfc4f22c3c1dd025ae157a9d
Ruthie <ruthienewman@protonmail.com>
2022-03-09 20:15:26 -0800

    test: refactor max_sub_array unit tests to accomodate additional parameters of left and right added to the max_sub_array method in max_subarray.py.

--- tests/test_max_sub_array.py
+++ tests/test_max_sub_array.py
@@ -1,14 +1,16 @@
 from lib.max_subarray import max_sub_array
 
 def test_max_subarray_on_empty_array():
-    assert max_sub_array([]) == 0
+    
+    assert max_sub_array([], 0, 0) == 0
 
 def test_max_subarray_with_negative_elements():
     # Arrange
     input = [-3, -4, -5, -6, -7]
+    n = len(input)
 
     # Act
-    answer = max_sub_array(input)
+    answer = max_sub_array(input, 0, n-1)
 
     # Assert
     assert answer == -3
@@ -16,9 +18,10 @@ def test_max_subarray_with_negative_elements():
 def test_max_subarray_with_negative_array_with_largest_element_at_rear():
     # Arrange
     input = [-4, -5, -6, -7, -1]
+    n = len(input)
 
     # Act
-    answer = max_sub_array(input)
+    answer = max_sub_array(input, 0, n-1)
 
     # Assert
     assert answer == -1
@@ -26,9 +29,10 @@ def test_max_subarray_with_negative_array_with_largest_element_at_rear():
 def test_max_subarray_with_one_element_array():
     # Arrange
     input = [3]
+    n = len(input)
 
     # Act
-    answer = max_sub_array(input)
+    answer = max_sub_array(input, 0, n-1)
 
     # Assert
     assert answer == 3
@@ -36,9 +40,10 @@ def test_max_subarray_with_one_element_array():
 def test_max_sub_array_with_50_neg_50_50():
     # Arrange
     input = [50, -50, 50]
+    n = len(input)
 
     # Act
-    answer = max_sub_array(input)
+    answer = max_sub_array(input, 0, n-1)
 
     # Assert
     assert answer == 50
@@ -46,19 +51,22 @@ def test_max_sub_array_with_50_neg_50_50():
 def test_max_sub_array_with_50_3_neg_50_3():
     # Arrange
     input = [50, 3, -50, 50, 3]
+    n = len(input)
 
     # Act
-    answer = max_sub_array(input)
+    answer = max_sub_array(input, 0, n-1)
 
     # Assert
     assert answer == 56
 
 def test_max_sub_array_with_50_3_neg_50_10_65_neg_3():
-        # Arrange
+    # Arrange
     input = [50, 3, -50, 10, 65, -3]
+    n = len(input)
 
     # Act
-    answer = max_sub_array(input)
+    answer = max_sub_array(input, 0, n-1)
 
     # Assert
     assert answer == 78 # 50, 3, -50, 10, 65 (largest subarray)
+    
